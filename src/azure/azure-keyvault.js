8dd85cf707bba77486ef5e0192cd987aa9c1d687
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-03-27 13:04:58 -0700

    Diberry/0327 keyvault fixes (#7)
    * Key Vault instruction changes
    
    * Move instructions to top of file

--- src/azure/azure-keyvault.js
+++ src/azure/azure-keyvault.js
@@ -1,3 +1,78 @@
+/* 
+* Assumes Key Vault exists already.
+* 1. Create a resource group for all Azure resources in app
+*    or project - skip if you have already completed for 
+*    this sample app
+* 
+* 2. Create an Azure Key Vault resource in Azure CLI
+* 
+*        az keyvault create \
+*        --subscription REPLACE-WITH-YOUR-SUBSCRIPTION-NAME-OR-ID \
+*        --resource-group REPLACE-WITH-YOUR-RESOURCE-GROUP-NAME \
+*        --name REPLACE-WITH-YOUR-KEY-VAULT-NAME
+* 
+* 3. Create Service Principal (LOGICAL-APP-NAME) for new App 
+*    registration with the following Azure CLI command 
+*    in a bash terminal: 
+* 
+*        az ad sp create-for-rbac \
+*       --name REPLACE-WITH-YOUR-NEW-APP-LOGICAL-NAME
+*       --skip-assignment 
+* 
+*       ServicePrincipalOutput = 
+* 
+*       {
+*           "appId": "123456",
+*           "displayName": "REPLACE-WITH-YOUR-NEW-APP-LOGICAL-NAME",
+*           "name": "http://REPLACE-WITH-YOUR-NEW-APP-LOGICAL-NAME",
+*           "password": "!@#$%",
+*           "tenant": "987654"
+*       }
+* 
+* 4. Give Service Principal (LOGICAL-APP-NAME) access to 
+*   Key Vault with Azure CLI command. The value for --spn is
+*   your `appId`.
+* 
+*       az keyvault set-policy \
+*      --subscription REPLACE-WITH-YOUR-SUBSCRIPTION-NAME-OR-ID \
+*      --name "REPLACE-WITH-YOUR-KEY-VAULT-NAME" \
+*      --spn REPLACE-WITH-YOUR-SERVICE-PRINCIPAL-APP-ID \
+*      --secret-permissions get list
+* 
+* 5. Add database connection string as secret named `DATABASEURL`.
+* 
+*      az keyvault secret set \
+*      --subscription REPLACE-WITH-YOUR-SUBSCRIPTION-NAME-OR-ID \
+*      --vault-name "REPLACE-WITH-YOUR-KEY-VAULT-NAME" \
+*      --name "DATABASEURL" \
+*      --value "mongodb://my-cosmos-mongodb"
+* 
+* 6. Set these environment variables in the `.env` file
+*    to create the REQUIRED 
+*    context to use DefaultAzureCredential.
+* 
+*    AZURE_TENANT_ID: The `tenant` in the JSON response above.
+*    AZURE_CLIENT_ID: The `appId` in the JSON response above.
+*    AZURE_CLIENT_SECRET: The `password` in the JSON response above.
+* 
+*    When you deploy the application to Azure app service, 
+*    you will also need to add this settings to your web app. 
+* 
+* 7. Set these environment variables in the `.env` file of the 
+*    sample project to programmatical determine which Key Vault 
+*    resource and secret to use.
+* 
+*    KEY_VAULT_NAME: The `tenant` in the JSON response above.
+*    KEY_VAULT_SECRET_NAME_DATABASEURL: The `appId` in the JSON response above.
+* 
+* 8. Call the getSecret function as
+* 
+*      const { getSecret } = require("./azure/azure-keyvault");
+*      const KEY_VAULT_CONNECTION_STRING_SECRET_NAME = "DATABASEURL";
+*      const KEY_VAULT_NAME = "my-keyvault";
+*      let DATABASE_URL = await getSecret(KEY_VAULT_CONNECTION_STRING_SECRET_NAME, KEY_VAULT_NAME);
+*/
+
 const { DefaultAzureCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
 
@@ -6,81 +81,6 @@ const getSecret = async (secretName, keyVaultName) => {
     if (!secretName || !keyVaultName) {
         throw Error("getSecret: Required params missing")
     }
-    
-    /* 
-    * Assumes Key Vault exists already.
-    * 1. Create a resource group for all Azure resources in app
-    *    or project - skip if you have already completed for 
-    *    this sample app
-    * 
-    * 2. Create an Azure Key Vault resource in Azure CLI
-    * 
-    *        az keyvault create \
-    *        --subscription REPLACE-WITH-YOUR-SUBSCRIPTION-NAME-OR-ID \
-    *        --resource-group REPLACE-WITH-YOUR-RESOURCE-GROUP-NAME \
-    *        --name REPLACE-WITH-YOUR-KEY-VAULT-NAME
-    * 
-    * 3. Create Service Principal (LOGICAL-APP-NAME) for new App 
-    *    registration with the following Azure CLI command 
-    *    in a bash terminal: 
-    * 
-    *        az ad sp create-for-rbac \
-    *       --name REPLACE-WITH-YOUR-NEW-APP-LOGICAL-NAME
-    *       --skip-assignment 
-    * 
-    *       ServicePrincipalOutput = 
-    * 
-    *       {
-    *           "appId": "123456",
-    *           "displayName": "REPLACE-WITH-YOUR-NEW-APP-LOGICAL-NAME",
-    *           "name": "http://REPLACE-WITH-YOUR-NEW-APP-LOGICAL-NAME",
-    *           "password": "!@#$%",
-    *           "tenant": "987654"
-    *       }
-    * 
-    * 4. Give Service Principal (LOGICAL-APP-NAME) access to 
-    *   Key Vault with Azure CLI command. The value for --spn is
-    *   your `appId`.
-    * 
-    *       az keyvault set-policy \
-    *      --subscription REPLACE-WITH-YOUR-SUBSCRIPTION-NAME-OR-ID \
-    *      --name "REPLACE-WITH-YOUR-KEY-VAULT-NAME" \
-    *      --spn REPLACE-WITH-YOUR-SERVICE-PRINCIPAL-APP-ID \
-    *      --secret-permissions get list
-    * 
-    * 5. Add database connection string as secret named `DATABASEURL`.
-    * 
-    *      az keyvault secret set \
-    *      --subscription REPLACE-WITH-YOUR-SUBSCRIPTION-NAME-OR-ID \
-    *      --vault-name "REPLACE-WITH-YOUR-KEY-VAULT-NAME" \
-    *      --name "DATABASEURL" \
-    *      --value "mongodb://my-cosmos-mongodb"
-    * 
-    * 6. Set these environment variables in the `.env` file
-    *    to create the REQUIRED 
-    *    context to use DefaultAzureCredential.
-    * 
-    *    AZURE_TENANT_ID: The `tenant` in the JSON response above.
-    *    AZURE_CLIENT_ID: The `appId` in the JSON response above.
-    *    AZURE_CLIENT_SECRET: The `password` in the JSON response above.
-    * 
-    *    When you deploy the application to Azure app service, 
-    *    you will also need to add this settings to your web app. 
-    * 
-    * 7. Set these environment variables in the `.env` file of the 
-    *    sample project to programmatical determine which Key Vault 
-    *    resource and secret to use.
-    * 
-    *    KEY_VAULT_NAME: The `tenant` in the JSON response above.
-    *    KEY_VAULT_SECRET_NAME_DATABASEURL: The `appId` in the JSON response above.
-    * 
-    * 8. Call the getSecret function as
-    * 
-    *      const { getSecret } = require("./azure/azure-keyvault");
-    *      const KEY_VAULT_CONNECTION_STRING_SECRET_NAME = "DATABASEURL";
-    *      const KEY_VAULT_NAME = "my-keyvault";
-    *      let DATABASE_URL = await getSecret(KEY_VAULT_CONNECTION_STRING_SECRET_NAME, KEY_VAULT_NAME);
-    */
     
     if (!process.env.AZURE_TENANT_ID ||
         !process.env.AZURE_CLIENT_ID ||
